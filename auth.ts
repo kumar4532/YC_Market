3d80c489ec68432e3381624b5a4b2921d555acc1
kumar4532 <krantikumar2001@gmail.com>
2024-12-09 20:40:06 +0530

    creating authors

--- auth.ts
+++ auth.ts
@@ -1,6 +1,40 @@
 import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
+import { client } from "./sanity/lib/client"
+import { AUTHOR_BY_GOOGLE_ID_QUERY } from "./sanity/lib/queries"
+import { writeClient } from "./sanity/lib/write-client"
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Google],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const existingUser = await client.withConfig({useCdn: false}).fetch(AUTHOR_BY_GOOGLE_ID_QUERY, { id: profile?.sub })
+
+      if (!existingUser) {
+        await writeClient.create({
+          _type: 'author',
+          id: profile?.sub,
+          name: user?.name,
+          username: profile?.login,
+          email: user?.email,
+          image: user?.image,
+          bio: profile?.bio || ''
+        })
+      }
+
+      return true
+    },
+    async jwt({ token, account, profile }) {
+      if (account && profile) {
+        const user = await client.withConfig({useCdn: false}).fetch(AUTHOR_BY_GOOGLE_ID_QUERY, { id: profile?.sub })
+        token.id = user?._id
+      }
+
+      return token
+    },
+    async session({ session, token }) {
+      Object.assign(session, { id: token?.id });
+      return session
+    }
+  }
 })
